2f5bf10ea680710c91efb01a8852c8d8eaf65f1f
Gabriel Antelo <gabriel.antelo@southworks.com>
2020-06-23 10:20:30 -0300

    Updates

---
+++ precache-manifest.c89577277f871f2698aa24722ab11ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6f95604b7fc28d3f7ab3ae4aae02a2",
+    "url": "/emojicommit/index.html"
+  },
+  {
+    "revision": "94ae008445c5002060a9",
+    "url": "/emojicommit/static/css/main.3263926d.chunk.css"
+  },
+  {
+    "revision": "01112c40e699a6fdbbed",
+    "url": "/emojicommit/static/js/2.e1d186f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/emojicommit/static/js/2.e1d186f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ae008445c5002060a9",
+    "url": "/emojicommit/static/js/main.64913aff.chunk.js"
+  },
+  {
+    "revision": "7a6067cffb535fdd4c87",
+    "url": "/emojicommit/static/js/runtime-main.ce372aaa.js"
+  }
+]);
